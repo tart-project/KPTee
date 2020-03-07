906ca0f93f5229d862c9b4748aa96dcb7a254ad2
shimoseko.yuta <yuta.s0723@gmail.com>
2020-03-07 19:51:11 +0900

    add color picker

--- src/color-picker.js
+++ src/color-picker.js
@@ -4,23 +4,33 @@ export default class ColorsPicker {
 	constructor() {
 		this.colors = colors
 		// ピッカーを表示しているカードを特定するために保持
-		this.colorsFlagId = ""
+		this.pickerId = ""
 	}
 
 	// pickerが表示中に他要素をクリックした場合にpickerを非表示
 	checkClickedPoint(e, whiteboard) {
-		if (e.target.className == "changeColorButtonImg" || e.target.className == "picker" || e.target.className == "colors") { return }
-		if (this.colorsFlagId != "") {
-			let target = whiteboard.cards.find(({ id }) => id === this.colorsFlagId)
-			if (target.colorPickerFlag) {
-				target.colorPickerFlag = false
+		if (e.target.className == "imgOnCard" || e.target.className == "picker" || e.target.className == "colors") {
+			// ピッカー、カラーが押された場合はリターン
+			return
+		}
+		if (this.pickerId != "") {
+			let target = whiteboard.cards.find(({ id }) => id === this.pickerId)
+			if (target.pickerShowOrHideFlag) {
+				target.pickerShowOrHideFlag = false
+				this.pickerId = ""
 			}
 		}
 	}
 
 	showAndHide(e, whiteboard) {
 		let target = whiteboard.cards.find(({ id }) => id === e.target.parentNode.parentNode.id)
-		target.colorPickerFlag = !target.colorPickerFlag
-		this.colorsFlagId = target.id
+		if (this.pickerId != "" && this.pickerId != target.id) {
+			whiteboard.cards.find(({ id }) => id === this.pickerId).pickerShowOrHideFlag = false
+		}
+		else if (this.pickerId != "" && this.pickerId == target.id) {
+			this.pickerId = ""
+		}
+		target.pickerShowOrHideFlag = !target.pickerShowOrHideFlag
+		this.pickerId = target.id
 	}
 }
