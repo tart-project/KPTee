88b34c4b38b817274b55bde6d54a068566cf79f4
shimoseko.yuta <shimoseko.yuta@jp.fujitsu.com>
2020-03-18 13:26:08 +0900

    add web server

--- server/web-server.js
+++ server/web-server.js
@@ -11,7 +11,8 @@ exports.run = function () {
             filePath = './index.html';
         }
 
-        const extname = String(path.extname(filePath)).toLowerCase();
+        const fileExtensionName = String(path.extname(filePath)).toLowerCase();
+
         const mimeTypes = {
             '.html': 'text/html',
             '.js': 'text/javascript',
@@ -20,26 +21,11 @@ exports.run = function () {
             '.png': 'image/png'
         };
 
-        const contentType = mimeTypes[extname] || 'application/octet-stream';
+        const contentType = mimeTypes[fileExtensionName] || 'application/octet-stream';
 
         fileSystem.readFile(filePath, function (error, content) {
-            if (error) {
-                if (error.code == 'ENOENT') {
-                    fileSystem.readFile('./404.html', function (error, content) {
-                        response.writeHead(200, { 'Content-Type': contentType });
-                        response.end(content, 'utf-8');
-                    });
-                }
-                else {
-                    response.writeHead(500);
-                    response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
-                    response.end();
-                }
-            }
-            else {
                 response.writeHead(200, { 'Content-Type': contentType });
                 response.end(content, 'utf-8');
-            }
         });
         // TODO：ポート番号の検討
     }).listen(8080);
