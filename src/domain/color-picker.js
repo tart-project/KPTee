c0e9e9a6bd81322bb3361d74c53faf9d5d5e3d37
shimoseko.yuta <shimoseko.yuta@jp.fujitsu.com>
2020-03-19 17:56:10 +0900

    introduce websocket

--- src/domain/color-picker.js
+++ src/domain/color-picker.js
@@ -3,24 +3,5 @@ import { colors } from '../constant'
 export default class ColorsPicker {
 	constructor() {
 		this.colors = colors
-		// ピッカーを表示しているカードを特定するために保持
-		this.pickersFlagId = ""
-	}
-
-	// pickerが表示中に他要素をクリックした場合にpickerを非表示
-	checkClickedPoint(e, whiteboard) {
-		if (e.target.className == "imagesOnCard" || e.target.className == "picker" || e.target.className == "pickers") { return }
-		if (this.pickersFlagId != "") {
-			let target = whiteboard.cards.find(({ id }) => id === this.pickersFlagId)
-			if (target.colorPickerFlag) {
-				target.colorPickerFlag = false
-			}
-		}
-	}
-
-	showAndHide(e, whiteboard) {
-		let target = whiteboard.cards.find(({ id }) => id === e.target.parentNode.parentNode.id)
-		target.colorPickerFlag = !target.colorPickerFlag
-		this.pickersFlagId = target.id
 	}
 }
